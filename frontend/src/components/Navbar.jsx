e9d5aedf8a56897dd9b4d00044f769812ec034d6
lokes <lokeswar5656@gmail.com>
2025-03-06 21:40:08 +0530

    routes to pages

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      
+      <img src=
+
+    </div>
+  )
+}
+
+export default Navbar
